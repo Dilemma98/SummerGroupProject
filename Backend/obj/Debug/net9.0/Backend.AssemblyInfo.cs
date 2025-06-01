da4277af34c121544755713e39fee0d818cb4f1b
Dilemma98 <emmaah98@outlook.com>
2025-06-01 21:45:30 +0200

    Now post is added FOR REAL

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ee76708c58118d6174faab67c1953a6c3ef710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddf030dd7918974686153fb773709ab7df665ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
