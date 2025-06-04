e0e1b66443b83ccca70539d181b61785f02a75e5
Dilemma98 <emmaah98@outlook.com>
2025-06-04 14:53:51 +0200

    added support for images in backend

--- Backend/Models/Post.cs
+++ Backend/Models/Post.cs
@@ -5,6 +5,7 @@ namespace Backend.Models
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
+        public string? ImageUrl { get; set; }
         public string? Author { get; set; }
         public DateTime CreatedAt { get; set; }
     }
