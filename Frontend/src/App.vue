06470d3748f87d8e48b74afa1c55601c0772a8fc
Dilemma98 <emmaah98@outlook.com>
2025-06-18 19:51:02 +0200

    Made client work, you can now add new posts

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
-import CreatePost from './components/CreatePost.vue';
 import Header from './components/Header.vue';
-import ListPosts from './components/ListPosts.vue';
 </script>
 
 <template>
   <div>
     <Header />
-    <CreatePost />
-    <ListPosts />
+    <router-link to="/create"></router-link>
+    <router-view /> <!-- Här visas rätt sida beroende på route -->
   </div>
 </template>
+<style>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- Frontend/src/components/Header.vue
+++ Frontend/src/components/Header.vue
@@ -3,13 +3,25 @@
 </script>
 
 <template>
-  <header class="header">
-    <h1 class="logo">Tramsrapporten</h1>
-    <p>Header</p>
+  <header class="newspaper-header">
+    <h1 class="newspaper-title">Tramsrapporten</h1>
   </header>
 </template>
 
-<style>
+<style scoped>
+@import url('https://fonts.cdnfonts.com/css/unifrakturmaguntia');
+.newspaper-header {
+  border-bottom: 2px solid #333;
+  margin-bottom: 2rem;
+  margin-top: -1rem;
+  width: 90%;
+  margin: auto;
+}
 
+.newspaper-title {
+  font-family: 'UnifrakturMaguntia', Times, serif;
+  font-size: 5rem;
+  text-align: center;
+  color: #111;
+}
 </style>
-
