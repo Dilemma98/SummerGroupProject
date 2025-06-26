c44901a8bec64b3ac752a4d13561110de81acf8d
Dilemma98 <emmaah98@outlook.com>
2025-06-26 16:12:04 +0200

    login made, using googleLogin

--- Frontend/src/components/Header.vue
+++ Frontend/src/components/Header.vue
@@ -1,27 +1,65 @@
 <script setup lang="ts">
+import GoogleLoginButton from './googleLoginButton.vue';
+import LogoutButton from './logoutButton.vue';
+import { useUser } from '../composables/useUser';
+const { user } = useUser();
 
+
+console.log('User from header:', user.value); // Logga användaren för felsökning
 </script>
 
 <template>
   <header class="newspaper-header">
-    <h1 class="newspaper-title">Tramsrapporten</h1>
+    <router-link to="/" class="newspaper-title">Tramsrapporten</router-link>
+    <div v-if="!user || !user.email" class="button-container">
+      <GoogleLoginButton />
+    </div>
+    <div v-if="user && user.email" class="welcomeUserText">
+      <h3>Välkommen {{user.name}}! <img :src="user.picture" alt=""></h3>
+    </div>
+    <LogoutButton v-if="user && user.email" />
   </header>
 </template>
 
 <style scoped>
 @import url('https://fonts.cdnfonts.com/css/unifrakturmaguntia');
+
 .newspaper-header {
   border-bottom: 2px solid #333;
   margin-bottom: 2rem;
   margin-top: -1rem;
   width: 90%;
   margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; /* Lägg till denna rad */
+  gap: 2rem; /* Lägg till mellanrum mellan elementen */
 }
 
 .newspaper-title {
-  font-family: 'UnifrakturMaguntia', Times, serif;
+  font-family: 'UnifrakturMaguntia', cursive;
   font-size: 5rem;
-  text-align: center;
-  color: #111;
+  color: #333;
+  margin: 0;
+}
+
+.button-container {
+  margin: 0; /* Ta bort stor marginal så knappen inte trycks ut */
+}
+
+.welcomeUserText {
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-size: 1.2rem;
+  color: #333;
+  margin-left: 7em;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+img {
+  border-radius: 50%;
+  height: 2.5em;
+  margin-left: 0.5em;
 }
 </style>
